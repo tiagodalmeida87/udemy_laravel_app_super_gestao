ad45e67f3344c2d0641dd15b6d854e82f4213449
Tiago Almeida <tiagodalmeida87@hotmail.com>
2022-10-31 21:42:25 -0300

    adjust layout app

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,22 @@
+<h3>Fornecedores</h3>
+
+{{-- Comentário em blade Laravel - Fica o comentário em blade --}}
+
+{{-- 
+    {{ 'Texto de teste' }}
+
+    <?='Texto de teste' ?>
+    
+    @php
+     // Serve para comentar 1 linha
+
+     /*
+        Para comentar várias linhas 
+     */
+
+
+        echo 'Texto de teste';
+    @endphp
+
+    
+    --}}
--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
 Route::get('/sobre-nos',[\App\Http\Controllers\SobreNosController::class, 'principal'])->name('site.sobrenos');
 Route::get('/contato',[\App\Http\Controllers\ContatoController::class, 'principal'])->name('site.contato');
+Route::post('/contato',[\App\Http\Controllers\ContatoController::class, 'principal'])->name('site.contato');
 Route::get('/login', function(){return 'Login';})->name('site.login');
 
 Route::prefix('/app')->group(function() {
     Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
-    Route::get('/fornecedores', function(){return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/fornecedores', [\App\Http\Controllers\FornecedorController::class, 'index'])->name('app.fornecedores');
     Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
 });
 
